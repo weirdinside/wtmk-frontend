6e70672ccd7e3f1640c7c0d041464361669ea230
weirdinside <ani@weirdinsi.de>
2024-10-16 08:04:37 -0400

    working changes to build

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/wtmk-frontend">
       <App />
     </BrowserRouter>
   </StrictMode>,
